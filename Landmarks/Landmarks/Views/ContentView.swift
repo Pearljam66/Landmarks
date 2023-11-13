7034d9ece654e7a5391034677b7e65b0d9f10e13
Sarah Clark <sarahireneclark@gmail.com>
2023-11-13 17:01:49 -0600

    Code up to the Animating views and transitions chapter

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Sarah Clark on 11/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
+
---
+++ Landmarks/Landmarks/Views/Helper/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImageView.swift
+//  Landmarks
+//
+//  Created by Sarah Clark on 11/11/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage(image: Image("turtlerock"))
+}
