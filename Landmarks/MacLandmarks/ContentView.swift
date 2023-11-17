557e63de0957cffe50cfd21cc01c44b45250db4f
Sarah Clark <sarahireneclark@gmail.com>
2023-11-16 21:06:17 -0600

    Finished "Creating a macOS app" section and tutorial.

---
+++ Landmarks/MacLandmarks/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Sarah Clark on 11/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
